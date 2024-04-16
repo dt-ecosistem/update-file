21a7ba9b382e4066fbdd35ba1cca4e43b0032bf2
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-04-16 17:14:32 +0500

    s

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,47 @@
 <template>
-
   <div class="flex items-center justify-center w-full mt-3.5">
-    <div  class="">
+    <div class="">
       <label for="dropzone-file" class="flex flex-col items-center justify-center w-[500px] h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div v-if="loading" class="text-white w-full h-full " >
-
-<!--          <img :src="data" alt="salom">-->
-
-              <img :src="`http://192.168.100.241:9999/api/file/view-image/${data}`" alt="salom" class="w-full  h-full">
-
+        <div v-if="loading" class="text-white w-full h-full ">
+          <img :src="`http://192.168.100.241:9999/api/file/view-image/${data}`" alt="salom" class="w-full  h-full">
         </div>
         <div v-else class="flex flex-col items-center justify-center pt-5 pb-6">
           <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
           </svg>
           <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-
         </div>
-        <input id="dropzone-file" @input="setData" type="file" class="hidden" />
+        <input id="dropzone-file" @change="xx" type="file" class="hidden" />
       </label>
     </div>
   </div>
-
+  <button @click="setData" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
 </template>
 
-
 <script setup lang="ts">
-import {fechimg, upload} from "./components/api.ts";
-import {ref} from "vue";
+import { fechimg, upload } from "./components/api.ts";
+import { ref } from "vue";
+
 const loading = ref(false);
 const data = ref(null);
+const file = ref(null);
 
-// olish
 const fechImgdata = async (responseData: any) => {
   try {
     const image = await fechimg(responseData);
-// data.value=image;
-    // console.log("Olingan rasm:", image);
+    console.log("Olingan rasm:", image);
     console.log(data)
   } catch (error) {
     console.error("Rasmni olishda xatolik yuz berdi:", error);
   }
 }
 
-// jonatish
+const xx = (e:any) => {
+  file.value = e.target.files[0];
+}
+
 const setData = async (event: any) => {
-  const fileType = event.target.files[0];
+  const fileType = file.value;
   if (fileType) {
     try {
       const formdata = new FormData();
@@ -56,7 +52,9 @@ const setData = async (event: any) => {
       const response = await upload(formdata);
       loading.value = true;
       console.log("as",response);
-      data.value=response.objectName;
+
+      data.value = response.objectName;
+
       await fechImgdata(response.objectName);
     } catch (error) {
       console.error("Xatolik yuz berdi:", error);
@@ -64,7 +62,6 @@ const setData = async (event: any) => {
   }
   console.log("Rasmning turi:", fileType);
 }
-
 </script>
 
 <style scoped></style>
